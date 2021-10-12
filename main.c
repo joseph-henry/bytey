e2cd49a2a6e66063fabfbed966f624a2b74c4c65
Joseph Henry <joseph.henry@zerotier.com>
2021-10-11 21:58:27 -0700

    byte

--- main.c
+++ main.c
@@ -2,15 +2,15 @@
 
 int main(int argc, char **argv) {
 
-   /*
    // Trigger undefined behaviour sanitizer
    int k = 0x7fffffff;
    k += argc;
-   */
 
+   /*
    // Trigger address sanitizer (also memory sanitizer)
    int array[1];
    int y = array[1];
+   */
 
    printf("nom\n");
    return 0;
